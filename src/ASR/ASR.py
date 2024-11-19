da6799f84e72161cfa028a43e43b3042296797e1
Zongheng Wu <wzh1719592316@gmail.com>
2024-11-18 19:01:18 -0500

    Update ASR.py

--- src/ASR/ASR.py
+++ src/ASR/ASR.py
@@ -1,6 +1,11 @@
 from openai import OpenAI
 from pathlib import Path
+from PIL import Image
+import cv2
+from collections import Counter
+from pathlib import Path
 
+import clip
 import logging
 import torch
 import stable_whisper
@@ -23,6 +28,21 @@ def get_transcript(method, src_srt_path, source_lang, audio_path, client, task_l
         elif method == "whisper-large-v3":
             transcript = get_transcript_whisper_large_v3(audio_path, pre_load_asr_model)
             is_trans = True
+        elif method == "whisper_clips":
+            # File path
+            file_path = "/content/categories_places365.txt"
+            category_list = []
+            with open(file_path, "r") as file:
+                for line in file:
+                    category_path = line.strip().split(" ")[0][3:]
+                    categories = category_path.split("/")
+                    category_list=category_list+categories
+            modified_category_list_2=[]
+            for words in category_list:
+                categories=words.replace("_"," ")
+                modified_category_list_2.append(categories)
+                category_database= list(set(modified_category_list_2))
+        
         elif "stable" in method:
             whisper_model = method.split("-")[2]
             transcript = get_transcript_stable(audio_path, whisper_model, init_prompt, pre_load_asr_model)
